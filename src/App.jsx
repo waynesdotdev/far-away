5c1d7858d376b45c00dd8ed1bc24fae6070d28b2
waynesdotdev <fox.wayne1@gmail.com>
2023-06-07 09:03:21 +0100

    stats showing

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,29 @@ export default function App() {
     setItems(items => items.filter(item => item.id !== id))
   }
 
+  function handleToggleItem(id) {
+    setItems(items =>
+      items.map(item =>
+        item.id === id
+          ? {
+              ...item,
+              packed: !item.packed,
+            }
+          : item
+      )
+    )
+  }
+
   return (
     <div className='app'>
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
-      <Stats />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   )
 }
